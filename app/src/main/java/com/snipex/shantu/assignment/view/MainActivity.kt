0569d1bc829a184decfb4c851a2740693d94d312
ingaleashish <58884070+ingaleashish@users.noreply.github.com>
2020-04-07 13:00:13 +0530

    Update MainActivity.kt

--- app/src/main/java/com/snipex/shantu/assignment/view/MainActivity.kt
+++ app/src/main/java/com/snipex/shantu/assignment/view/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.snipex.shantu.assignment.view
 
 import android.os.Bundle
-import androidx.annotation.NonNull
+import androidx.annotation.NonNull 
 import androidx.annotation.Nullable
 import androidx.annotation.VisibleForTesting
 import androidx.appcompat.app.AppCompatActivity
