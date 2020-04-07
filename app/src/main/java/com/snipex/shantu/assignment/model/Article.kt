143a09f9eeec168188293959ce994db2135b46a5
ingaleashish <58884070+ingaleashish@users.noreply.github.com>
2020-04-07 12:58:08 +0530

    Update Article.kt

--- app/src/main/java/com/snipex/shantu/assignment/model/Article.kt
+++ app/src/main/java/com/snipex/shantu/assignment/model/Article.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 class Article {
 
-    var title: String? = null
+    var title: String? = null 
     @SerializedName("imageHref")
     var urlToImage: String? = null
     var description: String? = null
