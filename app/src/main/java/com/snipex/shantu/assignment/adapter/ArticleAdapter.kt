5f17f997c0ffcc6c9b2aaaebd78d0db0abec44b2
ingaleashish <58884070+ingaleashish@users.noreply.github.com>
2020-04-07 12:57:23 +0530

    Update ArticleAdapter.kt

--- app/src/main/java/com/snipex/shantu/assignment/adapter/ArticleAdapter.kt
+++ app/src/main/java/com/snipex/shantu/assignment/adapter/ArticleAdapter.kt
@@ -3,7 +3,7 @@ package com.snipex.shantu.assignment.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.view.ViewGroup 
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
